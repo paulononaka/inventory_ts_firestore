f6359f8dd937d791cc989b8c724e4828584ab566
Paulo Henrique Nonaka <paulo.nonaka@ifood.com.br>
2021-11-17 05:09:00 -0300

    Setup Cloud Functions

---
+++ docs/adr/0002-firebase-for-hosting-and-storage.md
@@ -0,0 +1,7 @@
+# firebase-for-hosting-and-storage
+
+Date: 2021-11-17
+
+## Decision
+
+We will be using Firebase Cloud Functions for hosting the APIs and Firebase Cloud Firestore for storage because it is very simple to use, it is very scalable and it will allows us to do demonstrate a more complete and functional sample of a real API.
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+const expressServer = express();
+
+const createFunction = async (expressInstance): Promise<void> => {
+  const initialized = admin.apps.some((app) => app.name === '[DEFAULT]');
+  if (!initialized) {
+    admin.initializeApp();
+  }
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  await app.init();
+};
+
+export const api = functions
+  .region('us-central1')
+  .https.onRequest(async (request, response) => {
+    await createFunction(expressServer);
+    expressServer(request, response);
+  });
